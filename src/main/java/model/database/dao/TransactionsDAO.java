7c928a55af4986aad0e18a4ee26ab9ae9f030edf
duo <duoxu1987@gmail.com>
2023-06-15 16:31:40 +0200

    Finished remaining todo on TransactionsDAO

--- src/main/java/model/database/dao/TransactionsDAO.java
+++ src/main/java/model/database/dao/TransactionsDAO.java
@@ -10,8 +10,9 @@ import model.database.containers.Transactions.TransactionValues;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.TreeMap;
 
 public class TransactionsDAO extends DAO {
     private static TransactionsDAO instance;
@@ -48,7 +49,7 @@ public class TransactionsDAO extends DAO {
             while (resultSet.next()) {
                 tmpTicker = TransactionPK.Ticker.getInstance(resultSet.getString(TICKER_POS));
 
-                if (!tmpMap.isCompatible(tmpTicker)){
+                if (!tmpMap.isCompatible(tmpTicker)) {
                     portfolio.put(tmpTicker, tmpMap);
                     tmpMap = new TransactionMap(tmpTicker);
                 }
@@ -64,15 +65,46 @@ public class TransactionsDAO extends DAO {
         return portfolio;
     }
 
-    public TransactionMap getAllTransactionsOnTicker(String FK_Ticker_DailyAssets) {
-        return null;
+    public TransactionMap getAllTransactionsOnTicker(PortfolioPK portfolioPK, TransactionPK.Ticker ticker) {
+        SQLQuery sqlQuery = SQLQuery.SELECT_TRANSACTION_MAPS_ON_PORTFOLIO_BY_TICKER;
+        TransactionMap transactionMap = new TransactionMap(ticker);
+        final int DATE_POS = 0, AMOUNT_POS = 1;
+        try (ResultSet resultSet = executeQuery(sqlQuery, portfolioPK.getUUID(), ticker.getTicker())) {
+            while (resultSet.next()) {
+                transactionMap.put(ticker, TransactionPK.parseDate(resultSet.getString(DATE_POS)),
+                        TransactionValues.getInstance(resultSet.getDouble(AMOUNT_POS)));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return transactionMap;
     }
 
-    public TransactionMap.TransactionEntry getTransactionOnDate(Date date) {
-        return null;
+    public TransactionMap.TransactionEntry getTransactionOnDate(PortfolioPK portfolioPK, TransactionPK.Ticker ticker, Date date) {
+        SQLQuery sqlQuery = SQLQuery.SELECT_TRANSACTION_POINT;
+        TransactionMap.TransactionEntry entry;
+        final int AMOUNT_POS = 0;
+        try(ResultSet resultSet = executeQuery(sqlQuery, portfolioPK.getUUID(), ticker.getTicker(), date.toString())) {
+            resultSet.next();
+            entry = new TransactionMap.TransactionEntry(date, new TransactionValues(resultSet.getDouble(AMOUNT_POS)));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return entry;
     }
 
-    public TransactionMap.TransactionEntry getTransactionOnDate(String date, String format) {
-        return null;
+    public TransactionMap.TransactionEntry getTransactionOnDate(PortfolioPK portfolioPK, TransactionPK.Ticker ticker, String strDate, String format) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+
+        SQLQuery sqlQuery = SQLQuery.SELECT_TRANSACTION_POINT;
+        TransactionMap.TransactionEntry entry;
+        final int AMOUNT_POS = 0;
+        try(ResultSet resultSet = executeQuery(sqlQuery, portfolioPK.getUUID(), ticker.getTicker(), dateFormat.format(strDate))) {
+            resultSet.next();
+            entry = new TransactionMap.TransactionEntry(dateFormat.parse(strDate), new TransactionValues(resultSet.getDouble(AMOUNT_POS)));
+        } catch (SQLException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return entry;
     }
 }
